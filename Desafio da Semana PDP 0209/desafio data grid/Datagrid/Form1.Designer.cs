b8dd6f19df2dd6bfe01c18ae2313738912b3ca0c
Tiago Berlanda <79882301+tiagoberlanda@users.noreply.github.com>
2021-09-27 19:06:15 -0300

    Add files via upload

---
+++ Desafio da Semana PDP 0209/desafio data grid/Datagrid/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿
+namespace Datagrid
+{
+    partial class FormControleDeGastos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbDescricao = new System.Windows.Forms.TextBox();
+            this.tbValor = new System.Windows.Forms.TextBox();
+            this.tbTipo = new System.Windows.Forms.TextBox();
+            this.tbCategoria = new System.Windows.Forms.TextBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.dgControleDeGastos = new System.Windows.Forms.DataGridView();
+            this.colId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDescricao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colValor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTipo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCategoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colData = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btInserir = new System.Windows.Forms.Button();
+            this.dtpData = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dgControleDeGastos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Descrição:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(139, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Valor:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(274, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tipo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(390, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Categoria:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(512, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Data:";
+            // 
+            // tbDescricao
+            // 
+            this.tbDescricao.Location = new System.Drawing.Point(12, 56);
+            this.tbDescricao.Name = "tbDescricao";
+            this.tbDescricao.Size = new System.Drawing.Size(100, 20);
+            this.tbDescricao.TabIndex = 5;
+            // 
+            // tbValor
+            // 
+            this.tbValor.Location = new System.Drawing.Point(142, 56);
+            this.tbValor.Name = "tbValor";
+            this.tbValor.Size = new System.Drawing.Size(100, 20);
+            this.tbValor.TabIndex = 6;
+            // 
+            // tbTipo
+            // 
+            this.tbTipo.Location = new System.Drawing.Point(277, 56);
+            this.tbTipo.Name = "tbTipo";
+            this.tbTipo.Size = new System.Drawing.Size(100, 20);
+            this.tbTipo.TabIndex = 7;
+            // 
+            // tbCategoria
+            // 
+            this.tbCategoria.Location = new System.Drawing.Point(393, 56);
+            this.tbCategoria.Name = "tbCategoria";
+            this.tbCategoria.Size = new System.Drawing.Size(100, 20);
+            this.tbCategoria.TabIndex = 8;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(15, 108);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 10;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // dgControleDeGastos
+            // 
+            this.dgControleDeGastos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgControleDeGastos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colId,
+            this.colDescricao,
+            this.colValor,
+            this.colTipo,
+            this.colCategoria,
+            this.colData});
+            this.dgControleDeGastos.Location = new System.Drawing.Point(2, 156);
+            this.dgControleDeGastos.Name = "dgControleDeGastos";
+            this.dgControleDeGastos.Size = new System.Drawing.Size(797, 291);
+            this.dgControleDeGastos.TabIndex = 11;
+            // 
+            // colId
+            // 
+            this.colId.HeaderText = "Id";
+            this.colId.Name = "colId";
+            // 
+            // colDescricao
+            // 
+            this.colDescricao.HeaderText = "Descrição";
+            this.colDescricao.Name = "colDescricao";
+            this.colDescricao.Width = 200;
+            // 
+            // colValor
+            // 
+            this.colValor.HeaderText = "Valor";
+            this.colValor.Name = "colValor";
+            // 
+            // colTipo
+            // 
+            this.colTipo.HeaderText = "Tipo";
+            this.colTipo.Name = "colTipo";
+            // 
+            // colCategoria
+            // 
+            this.colCategoria.HeaderText = "Categoria";
+            this.colCategoria.Name = "colCategoria";
+            // 
+            // colData
+            // 
+            this.colData.HeaderText = "Data";
+            this.colData.Name = "colData";
+            // 
+            // btInserir
+            // 
+            this.btInserir.Location = new System.Drawing.Point(142, 108);
+            this.btInserir.Name = "btInserir";
+            this.btInserir.Size = new System.Drawing.Size(75, 23);
+            this.btInserir.TabIndex = 12;
+            this.btInserir.Text = "Inserir";
+            this.btInserir.UseVisualStyleBackColor = true;
+            this.btInserir.Click += new System.EventHandler(this.btInserir_Click);
+            // 
+            // dtpData
+            // 
+            this.dtpData.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpData.Location = new System.Drawing.Point(515, 56);
+            this.dtpData.Name = "dtpData";
+            this.dtpData.Size = new System.Drawing.Size(200, 20);
+            this.dtpData.TabIndex = 13;
+            // 
+            // FormControleDeGastos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dtpData);
+            this.Controls.Add(this.btInserir);
+            this.Controls.Add(this.dgControleDeGastos);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.tbCategoria);
+            this.Controls.Add(this.tbTipo);
+            this.Controls.Add(this.tbValor);
+            this.Controls.Add(this.tbDescricao);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormControleDeGastos";
+            this.Text = "Controle de Gastos";
+            this.Load += new System.EventHandler(this.FormControleDeGastos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgControleDeGastos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbDescricao;
+        private System.Windows.Forms.TextBox tbValor;
+        private System.Windows.Forms.TextBox tbTipo;
+        private System.Windows.Forms.TextBox tbCategoria;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.DataGridView dgControleDeGastos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDescricao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colValor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTipo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCategoria;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colData;
+        private System.Windows.Forms.Button btInserir;
+        private System.Windows.Forms.DateTimePicker dtpData;
+    }
+}
+
---
+++ Desafio da Semana PDP 0209/desafio data grid/Datagrid/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Datagrid
+{
+    public partial class FormControleDeGastos : Form
+    {
+        public FormControleDeGastos()
+        {
+            InitializeComponent();
+        }
+
+        private MySqlConnectionStringBuilder conexaoBanco()
+        {
+            MySqlConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+            conexaoBD.Server = "localhost";
+            conexaoBD.Database = "controlefinanceiro";
+            conexaoBD.UserID = "root";
+            conexaoBD.Password = "";
+            conexaoBD.SslMode = 0;
+            return conexaoBD;
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            limparCampos();
+        }
+
+        private void limparCampos()
+        {
+            tbDescricao.Clear();
+            tbValor.Clear();
+            tbTipo.Clear();
+            tbCategoria.Clear();
+            dtpData.Value = DateTime.Now;
+        }
+
+        private void FormControleDeGastos_Load(object sender, EventArgs e)
+        {
+            atualizarGrid();
+        }
+
+        private void atualizarGrid()
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+                comandoMySql.CommandText = "SELECT * FROM financas";
+                MySqlDataReader reader = comandoMySql.ExecuteReader();
+
+                dgControleDeGastos.Rows.Clear();
+
+                while (reader.Read())
+                {
+                    DataGridViewRow row = (DataGridViewRow)dgControleDeGastos.Rows[0].Clone();
+                    row.Cells[0].Value = reader.GetInt32(0);
+                    row.Cells[1].Value = reader.GetString(1);
+                    row.Cells[2].Value = reader.GetString(2);
+                    row.Cells[3].Value = reader.GetString(3);
+                    row.Cells[4].Value = reader.GetString(4);
+                    row.Cells[5].Value = reader.GetString(5);
+                    dgControleDeGastos.Rows.Add(row);
+                }
+
+                realizaConexacoBD.Close();
+
+            } catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection!");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void btInserir_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+                comandoMySql.CommandText = "INSERT INTO financas (descricaoFinanca, valorFinanca, tipoFinanca, categoriaFinanca, dataFinanca)" +
+                    "VALUES ('" + tbDescricao.Text + "'," + tbValor.Text + ",'" + tbTipo.Text + "','" + tbCategoria.Text + "','" + dtpData.Value.ToString("yyyy-MM-dd") + "')";
+
+                
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexacoBD.Close();
+                MessageBox.Show("Inserido com sucesso");
+                limparCampos();
+                atualizarGrid();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection!");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
